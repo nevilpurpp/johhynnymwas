6064fc95ff3e51b8de735d6c95e06da6ece42ab1
johhynnymwas <132211556+johhynnymwas@users.noreply.github.com>
2024-06-06 16:01:33 +0300

    Update pid.py

--- pid.py
+++ pid.py
@@ -47,8 +47,8 @@ def set_speed(motorA_speed, motorB_speed):
 def move_forward():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
 
 def stop():
     GPIO.output(IN1, GPIO.LOW)
